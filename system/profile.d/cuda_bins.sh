9b67c5db4da50d77ba12dc268a734a374243ff21
Tyler Spears <tylera117@gmail.com>
2021-10-20 12:38:44 -0400

    Remove spaces from cuda_bins profile

--- system/profile.d/cuda_bins.sh
+++ system/profile.d/cuda_bins.sh
@@ -7,12 +7,12 @@
 
 cuda_bin_path="/usr/local/cuda/bin"
 if [[ ! "$PATH" =~ "$cuda_bin_path" ]]; then
-    export PATH=${cuda_bin_path}${PATH:+:${PATH} }
+    export PATH=${cuda_bin_path}${PATH:+:${PATH}}
 fi
 
 cuda_lib_path="/usr/local/cuda/lib64"
 if [[ ! "$LD_LIBRARY_PATH" =~ "$cuda_lib_path" ]]; then
     export LD_LIBRARY_PATH=$cuda_lib_path\
-        ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH} }
+        ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 fi
 
